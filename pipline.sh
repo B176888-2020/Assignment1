0e5a7b6b59474eab707e3053c6c190b7739074ac
B176888-2020 <B176888-2020>
2020-10-19 11:44:31 +0100

    Server Version: add fastqc result dir and correct bed dir error
    Server Version: add fastqc result dir and correct bed dir error

--- pipline.sh
+++ pipline.sh
@@ -2,8 +2,10 @@
 
 # TODO: make it a program like bowtie2 that could be used in a command way
 mkdir -p ./data
+mkdir -p ./qcResult/
 mkdir -p ./refData
 mkdir -p ./interVar
+
 
 # Variables for different datasets
 # one-line method: samples=$(find ./data/ -type f -iname "*_1.fq.gz" -execdir sh -c 'printf "%s\n" "${0%_1.fq*}"' {} ';' | cut -d"/" -f2 | sort | uniq)
@@ -54,7 +56,7 @@ geneMean (){
 
 # Main 
 # Quick Check on the data by `fastqc`
-zcat ../data/*fq.gz | fastqc --outdir=./data/ stdin:fastqc_output
+zcat ../data/*fq.gz | fastqc --outdir=./qcResult/ stdin:fastqc_output
 
 # Uncompress the reference genome
 gunzip -c ../refData/Tb927_genome.fasta.gz > ./refData/Tb927_genome.fasta
@@ -75,11 +77,13 @@ find ./interVar/*.bam | parallel "samtools sort {} -o {}.sorted"
 find ./interVar/*.sorted | parallel "samtools index {}"
 
 # BEDtools to count the gene-aligned sequences
-find ./interVar/*.sorted | parallel "bedtools multicov -bams {} -bed ./refData/Tbbgenes.bed > {}.txt"
+find ./interVar/*.sorted | parallel "bedtools multicov -bams {} -bed ../refData/Tbbgenes.bed > {}.txt"
 
 # Gene Summary
 geneName=$(cat ./interVar/*.sam.bam.sorted.txt | awk '$5=="gene" {print $4}' | sort | uniq)
+
 > countStat.txt
+
 for gene in $geneName;
 do
     ((i=i%12)); ((i++==0)) && wait
