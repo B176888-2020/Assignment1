accc7f401dcabc48f83f75a7c189800c7c66efd6
B176888-2020 <B176888-2020>
2020-10-19 11:29:05 +0100

    Server Version: pathname modification

--- pipline.sh
+++ pipline.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# TODO: make it a program like bowtie2 that could be used in a command way
+# TODO: make it a program like bowtie2 that could be used in a command with options
 
+################ Preparation ################
 # Variables for different datasets
-# one-line method: samples=$(find ./data/ -type f -iname "*_1.fq.gz" -execdir sh -c 'printf "%s\n" "${0%_1.fq*}"' {} ';' | cut -d"/" -f2 | sort | uniq)
-samples=$(cat ./refData/fqfiles | awk '{print $3}' | cut -c -6 | sort | uniq)
-slender=$(cat ./refData/fqfiles | awk '$2=="Slender" {print $3}' | cut -c -6 | sort | uniq)
-stumpy=$(cat ./refData/fqfiles | awk '$2=="Stumpy" {print $3}' | cut -c -6 | sort | uniq)
+samples=$(cat /localdisk/data/BPSM/Assignment1/fastq/fqfiles | awk '{print $3}' | cut -c -6 | sort | uniq)
+slender=$(cat /localdisk/data/BPSM/Assignment1/fastq/fqfiles | awk '$2=="Slender" {print $3}' | cut -c -6 | sort | uniq)
+stumpy=$(cat /localdisk/data/BPSM/Assignment1/fastq/fqfiles | awk '$2=="Stumpy" {print $3}' | cut -c -6 | sort | uniq)
 
 # Preload Functions
 meanCal () {
@@ -14,7 +14,7 @@ meanCal () {
     sum=0;
     mean=0;
 
-    # Calculate the sum
+    # Calculate the sum of two types of dataset
     for sl in $1;
     do
     slCount=$(cat ./interVar/$sl.sam.bam.sorted.txt | awk -v gene="$2" '$4==gene {print $7}')
@@ -23,7 +23,7 @@ meanCal () {
     
     mean=$(awk -v suma="$sum" 'BEGIN{print suma/3}');
     
-    # Assign the mean value
+    # Assign the mean value to proper variable
     if [[ "$1" == "$slender" ]]
     then 
     slenderMean=$mean
@@ -31,7 +31,7 @@ meanCal () {
     then 
     stumpyMean=$mean
     else
-    echo "Unknown Type"
+    echo "Warning: Unknown Type"
     fi
 }
 
@@ -42,19 +42,20 @@ geneMean (){
     
     # Mean for slender samples and stumpy samples
     echo "Processing Gene $gene ...."
-    meanCal "$slender" "$gene"
-    meanCal "$stumpy" "$gene"
+    meanCal "$slender" "$gene" # Actually this function could get two values at the same time with additional tag column to distinct `slender` and `stumpy`
+    meanCal "$stumpy" "$gene" # However, it would require intermediate documents that may not be required in the assignment. To keep it simple, just keep it. 
 
     # Generate the output
     echo -e "${gene}\t${slenderMean}\t${stumpyMean}" >> countStat.txt
 }
 
-# Main 
+
+################ Main Pipeline Process ################
 # Quick Check on the data by `fastqc`
-zcat ./data/*fq.gz | fastqc stdin:fastqc_output
+zcat /localdisk/data/BPSM/Assignment1/fastq/*fq.gz | fastqc --outdir=./data/ stdin:fastqc_output
 
 # Uncompress the reference genome
-gunzip -c ./refData/Tb927_genome.fasta.gz > ./refData/Tb927_genome.fasta
+gunzip -c /localdisk/data/BPSM/Assignment1/Tbb_genome/Tb927_genome.fasta.gz > ./refData/Tb927_genome.fasta
 
 # Build the index for the reference genome
 bowtie2-build ./refData/Tb927_genome.fasta ./refData/Tb927_genome.fasta.index
@@ -63,7 +64,7 @@ for sample in $samples;
 do 
     # Use bowtie2 to 
     echo -e "Processing $sample..."
-    bowtie2 -p12 -x ./refData/Tb927_genome.fasta.index -1 "./data/${sample}_1.fq.gz" -2 "./data/${sample}_2.fq.gz" -S "./interVar/${sample}.sam"
+    bowtie2 -p12 -x ./refData/Tb927_genome.fasta.index -1 "/localdisk/data/BPSM/Assignment1/fastq/${sample}_1.fq.gz" -2 "/localdisk/data/BPSM/Assignment1/fastq/${sample}_2.fq.gz" -S "./interVar/${sample}.sam"
 done
 
 # Use samtools to convert .sam to sorted.bam and .bai files
